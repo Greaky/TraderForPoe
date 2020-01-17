4747828ef3de9a065ddb291247e7e829e2e7977c
Pault <samter.thomas@gmail.com>
2020-01-17 15:49:18 +0100

    Add Base .net Core Project

---
+++ .netCore/src/TraderForPoe.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TraderForPoe.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ TraderForPoe.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TraderForPoe.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
